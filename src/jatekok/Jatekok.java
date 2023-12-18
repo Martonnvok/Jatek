dd92348d2ca6f6ffd210bd6aa0b629da0c80d4c5
Martonnvok <martonsom@gmail.com>
2023-12-18 13:49:35 +0100

    git ignore beállítások

--- src/jatekok/Jatekok.java
+++ src/jatekok/Jatekok.java
@@ -5,17 +5,7 @@ public class Jatekok {
 
     
     public static void main(String[] args) {
-        Karakter karakter = new Karakter();
-
-        karakter.felvesz(new Targy("Fakard", "fakard leírás"), 2);
-        karakter.felvesz(new Targy("Bájital", "bájital Leírás"), 2);
-        karakter.felvesz(new Targy("Könyvek", "könyvek leírás"), 3);
-        System.out.println(karakter.toString());
-        karakter.hasznal("Fakard");
-        karakter.hasznal("Bájital", 2);
-        karakter.hasznal("Könyvek", 5, false);
-
-        System.out.println(karakter.toString());
+        
     }
     
 }
--- src/jatekok/Karakter.java
+++ src/jatekok/Karakter.java
@@ -1,68 +1,10 @@
 package jatekok;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Karakter {
 
     private int eletero, ugyesseg, szerencse;
     private ArrayList<Targy> felszereles;
-    private static final Random rnd = new Random();
-    private Targy t;
-
-    public Karakter() {
-        eletero = rnd.nextInt(1, 7) + rnd.nextInt(1, 7) + 12;
-        ugyesseg = rnd.nextInt(1, 7) + 6;
-        szerencse = rnd.nextInt(1, 7) + 6;
-        felszereles = new ArrayList<>();
-    }
-
-    public void felvesz(Targy targy) {
-        felvesz(targy, 1);
-    }
-
-    public void felvesz(Targy targy, int db) {
-        for (int i = 0; i < db; i++) {
-            felszereles.add(targy);
-        }
-    }
-
-    public void hasznal(String nev) {
-        hasznal(nev, 1);
-
-    }
-
-    public void hasznal(String nev, int db) {
-        hasznal(nev, db, true);
-    }
-
-    public void hasznal(String nev, int db, boolean levon) {
-        int i = felszereles.size() - 1;
-        int dbI = 0;
-        int szamlalo = 0;
-
-        while (i > -1) {
-            if (felszereles.get(i).getNev().equals(nev) && dbI < db) {
-                szamlalo++;
-                dbI++;
-                if (levon) {
-                    felszereles.remove(i);
-                }
-            }
-            i--;
-        }
-        if (szamlalo == 0) {
-            System.out.println(nev + " nincs ilyen tárgy");
-        } else if (levon && dbI == db || levon && db > szamlalo) {
-            System.out.println(dbI + " felhasznalva ebbol: " + nev);
-        } else if (levon == false) {
-            System.out.println(szamlalo + "/" + db + " nem lett felhasználva ebbol: " + nev);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Karakter{" + "felszereles=" + felszereles + '}';
-    }
 
 }
